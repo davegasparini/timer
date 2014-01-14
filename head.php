506f28ddf19b5e986ab66f3a2c7750e997e34a65
davegasparini <davegasparini@gmail.com>
2014-01-13 22:12:22 -0500

    renamed main js file to remain generic
    renamed main js file to remain generic

--- head.php
+++ head.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en-US">
  <head>
-  <script language="javascript" type="text/javascript" src="sim1.js?random=<?php echo uniqid(); ?>"></script>
+  <script language="javascript" type="text/javascript" src="timer.js?random=<?php echo uniqid(); ?>"></script>
   <script language="javascript" type="text/javascript" src="jquery-2.0.3.min.js"></script> 
  </head>
